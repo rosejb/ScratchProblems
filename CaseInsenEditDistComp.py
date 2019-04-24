c5eeda955eb7434f8b0993a537c3c516234380e4
James Rose <james.b.rose@vanderbilt.edu>
2019-04-24 08:25:03 -0500

    Added recursive implementation to perform the same case insensitive comparison within some edit distance.

--- CaseInsenEditDistComp.py
+++ CaseInsenEditDistComp.py
@@ -25,7 +25,34 @@ def case_insen_compare(s1, s2, tol):
 
     return False
 
+def case_insen_compare_rec(s1, s2, tol, mismatch_count = 0):
+    if abs(len(s1) - len(s2)) > tol - mismatch_count:
+        return False
+
+    s1, s2 = sorted([s1, s2], key=len)
+
+    for i in range(len(s1)):
+        if s1[0].lower() != s2[0].lower():
+            mismatch_count += 1
+
+            if mismatch_count > tol:
+                return False
+
+            if case_insen_compare_rec(s1, s2[1:], tol, mismatch_count):
+                return True
+
+    return len(s2) - len(s1) + mismatch_count <= tol
+
+
+print('Testing Iterative Implementation')
 print(case_insen_compare('abc', 'ABC', 1))  # True
 print(case_insen_compare('abc', 'def', 1))  # False
 print(case_insen_compare('abc', 'ABCD', 1))  # True
 print(case_insen_compare('abc', u'AC', 1))  # True
+
+
+print('\nTesting Recursive Implementation')
+print(case_insen_compare_rec('abc', 'ABC', 1))  # True
+print(case_insen_compare_rec('abc', 'def', 1))  # False
+print(case_insen_compare_rec('abc', 'ABCD', 1))  # True
+print(case_insen_compare_rec('abc', u'AC', 1))  # True
